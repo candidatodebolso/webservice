c8913de86d42cd60ef085a4343b2901ee59e2390
Welson <welsonlimawlsn@gmail.com>
2018-10-23 11:24:25 -0300

    Configuração application.properties

--- src/test/java/br/com/candidatodebolso/webservice/WebServiceApplicationTests.java
+++ src/test/java/br/com/candidatodebolso/webservice/WebServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WebserviceApplicationTests {
+public class WebServiceApplicationTests {
 
     @Test
     public void contextLoads() {
